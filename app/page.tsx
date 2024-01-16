db453c99d87a4b8042158122975487eecfe7f514
chany63 <chany63@korea.ac.kr>
2024-01-16 21:12:28 +0900

    Revert "feat: mainpage UI"
    This reverts commit 93f57a32e4fb6345c6ec7fc324273c9b4f418365.

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,5 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
-import ContentBox from "./components/contentBox";
-import FilterBox from "./components/filterBox";
-import IntroBox from "./components/introBox";
-import LoginButton from "./components/loginButton";
-import StylistProfileCard from "./components/stylistProfileCard";
-import StylistApplyButton from "./components/stylistApplyButton";
+import Image from "next/image";
 
 export default function Home() {
-  const [showScrolledLoginButton, setShowScrolledLoginButton] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } =
-        document.documentElement;
-
-      const scrollThreshold = 0.9;
-
-      if (scrollTop + clientHeight >= scrollHeight * scrollThreshold) {
-        setShowScrolledLoginButton(true);
-      } else {
-        setShowScrolledLoginButton(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  return (
-    <main className="flex min-h-screen w-full flex-col items-center bg-[#161616]">
-      <IntroBox />
-      <FilterBox />
-      <StylistProfileCard />
-      <StylistProfileCard />
-      <StylistProfileCard />
-      <ContentBox />
-      <StylistApplyButton isScrolled={showScrolledLoginButton} />
-      <LoginButton isScrolled={showScrolledLoginButton} />
-    </main>
-  );
+  return <main></main>;
 }
