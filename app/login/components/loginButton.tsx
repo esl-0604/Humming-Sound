9cf0b75cb235d4468e49307076a0559896f57dd4
chany63 <chany63@korea.ac.kr>
2024-01-16 21:19:28 +0900

    feat: loginpage UI

---
+++ app/login/components/loginButton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoginButton() {
+  return (
+    <div className="itmes-center absolute bottom-[30px] z-10 flex h-fit w-full justify-center px-[55px]">
+      <img src="/images/kakao_login_large_wide.png" alt="카카오 로그인 버튼" />
+    </div>
+  );
+}
