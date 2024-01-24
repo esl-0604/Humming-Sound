508b09ee4a775e0e48bc16fe7cfd61b927227531
chany63 <chany63@korea.ac.kr>
2024-01-24 16:46:00 +0900

    fix: login API

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import IntroBox from "./components/introBox";
 import LoginButton from "./components/loginButton";
+import { useEffect } from "react";
+import LocalStorage from "../utils/localstorage";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const param = useSearchParams();
+  const kakaoCode = param.get("code");
+  useEffect(() => {
+    const fetchData = async () => {
+      if (kakaoCode) {
+        try {
+          // Get Kakao Token
+          const loginResponse = await fetch("/api/kakao", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authCode: kakaoCode,
+            },
+          });
+          const data = await loginResponse.json();
+
+          if (data) {
+            LocalStorage.setItem("userId", data.userId);
+            router.replace("/");
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [kakaoCode]);
   return (
     <main className="relative flex min-h-screen w-full flex-col items-center bg-[#161616]">
       <IntroBox />
